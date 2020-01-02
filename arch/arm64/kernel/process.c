5ac2b3caeba88eeabf77f009ab8c96d760d8fd89
Amanieu d'Antras <amanieu@gmail.com>
2020-01-02 18:24:08 +0100

    UPSTREAM: arm64: Implement copy_thread_tls
    commit a4376f2fbcc8084832f2f114577c8d68234c7903 upstream.
    
    This is required for clone3 which passes the TLS value through a
    struct rather than a register.
    
    Change-Id: I71e8124905e5ffc574ef6e01aeb23a8dd9bd8f1d
    Signed-off-by: Amanieu d'Antras <amanieu@gmail.com>
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: <stable@vger.kernel.org> # 5.3.x
    Acked-by: Will Deacon <will@kernel.org>
    Link: https://lore.kernel.org/r/20200102172413.654385-3-amanieu@gmail.com
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -356,9 +356,8 @@ static inline void ropp_change_key(struct task_struct *p)
 }
 #endif
 
-int copy_thread(unsigned long clone_flags, unsigned long stack_start,
-		unsigned long stk_sz, struct task_struct *p)
-{
+int copy_thread_tls(unsigned long clone_flags, unsigned long stack_start,
+		unsigned long stk_sz, struct task_struct *p, unsigned long tls){
 	struct pt_regs *childregs = task_pt_regs(p);
 
 	memset(&p->thread.cpu_context, 0, sizeof(struct cpu_context));
@@ -390,11 +389,11 @@ int copy_thread(unsigned long clone_flags, unsigned long stack_start,
 		}
 
 		/*
-		 * If a TLS pointer was passed to clone (4th argument), use it
-		 * for the new thread.
+		 * If a TLS pointer was passed to clone, use it for the new
+		 * thread.
 		 */
 		if (clone_flags & CLONE_SETTLS)
-			p->thread.tp_value = childregs->regs[3];
+			p->thread.tp_value = tls;
 	} else {
 		memset(childregs, 0, sizeof(struct pt_regs));
 		childregs->pstate = PSR_MODE_EL1h;
