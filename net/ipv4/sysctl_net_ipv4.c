cb8f708fa508f098a1f6281b6e36fc9e02185a4d
fengkeqi <2321446134@qq.com>
2026-09-15 02:44:24 +0800

    sysctl: 补回 tcp_early_retrans 条目（与 netns 字段配套）

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -1144,6 +1144,14 @@ static struct ctl_table ipv4_net_table[] = {
 		.proc_handler   = proc_dointvec_minmax,
 		.extra1		= &three,
 		.extra2		= &hundred,	},
+		{
+		.procname	= "tcp_early_retrans",
+		.data		= &init_net.ipv4.sysctl_tcp_early_retrans,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= &zero,
+		.extra2		= &four,	},
 	{ }
 };
 
