98bcaa7de69aa1ec18a30dc8c2784eb225dbffdd
fengkeqi <2321446134@qq.com>
2026-09-14 12:42:10 +0800

    合并到257（实测可开机）

--- fs/file_table.c
+++ fs/file_table.c
@@ -489,6 +489,23 @@ void flush_delayed_fput_wait(void)
 	flush_delayed_work(&delayed_fput_work);
 }
 
+
+void fput_many(struct file *file, unsigned int refs)
+{
+	if (atomic_long_sub_and_test(refs, &file->f_count)) {
+		struct task_struct *task = current;
+
+		if (likely(!in_interrupt() && !(task->flags & PF_KTHREAD))) {
+			init_task_work(&file->f_u.fu_rcuhead, ____fput);
+			if (!task_work_add(task, &file->f_u.fu_rcuhead, true))
+				return;
+		}
+
+		if (llist_add(&file->f_u.fu_llist, &delayed_fput_list))
+			schedule_delayed_work(&delayed_fput_work, 1);
+	}
+}
+
 void fput(struct file *file)
 {
 	if (atomic_long_dec_and_test(&file->f_count)) {
