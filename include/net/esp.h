48ebaac3615f3892a1ceaebf1beb0d5383485fda
fengkeqi <2321446134@qq.com>
2026-09-14 15:20:53 +0800

    合并到297（实测可开机）

--- include/net/esp.h
+++ include/net/esp.h
@@ -7,8 +7,6 @@
 
 #include <linux/skbuff.h>
 
-#define ESP_SKB_FRAG_MAXSIZE (PAGE_SIZE << SKB_FRAG_PAGE_ORDER)
-
 struct ip_esp_hdr;
 
 static inline struct ip_esp_hdr *ip_esp_hdr(const struct sk_buff *skb)
--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -2056,7 +2056,7 @@ bool udp_sk_rx_dst_set(struct sock *sk, struct dst_entry *dst)
 	struct dst_entry *old;
 
 	if (dst_hold_safe(dst)) {
-		old = xchg(&sk->sk_rx_dst, dst);
+		old = xchg((__force struct dst_entry **)&sk->sk_rx_dst, dst);
 		dst_release(old);
 		return old != dst;
 	}
@@ -2255,7 +2255,7 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct udp_table *udptable,
 		// KNOX NPA - END
 #endif
 
-		if (unlikely(sk->sk_rx_dst != dst))
+		if (unlikely(rcu_dereference(sk->sk_rx_dst) != dst))
 			udp_sk_rx_dst_set(sk, dst);
 
 #ifdef CONFIG_KNOX_NCM
@@ -2522,7 +2522,7 @@ int udp_v4_early_demux(struct sk_buff *skb)
 
 	skb->sk = sk;
 	skb->destructor = sock_efree;
-	dst = READ_ONCE(sk->sk_rx_dst);
+	dst = rcu_dereference(sk->sk_rx_dst);
 
 	if (dst)
 		dst = dst_check(dst, 0);
