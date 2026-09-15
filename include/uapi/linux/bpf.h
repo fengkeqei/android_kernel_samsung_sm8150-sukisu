43e976e9f9540b59f2b432713df2839be8eceeb2
fengkeqi <2321446134@qq.com>
2026-09-15 19:28:02 +0800

    bpf: BPF_MAP_NAME_LEN 移到 union bpf_attr 之前（编译顺序修正）

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -237,6 +237,8 @@ enum bpf_attach_type {
 #define BPF_F_ACCESS_MASK	(BPF_F_RDONLY | BPF_F_WRONLY | \
 				 BPF_F_RDONLY_PROG | BPF_F_WRONLY_PROG)
 
+#define BPF_MAP_NAME_LEN 16
+
 union bpf_attr {
 	struct { /* anonymous struct used by BPF_MAP_CREATE command */
 		__u32	map_type;	/* one of enum bpf_map_type */
